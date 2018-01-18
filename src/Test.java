b4e36635e7b017e67606486f1888ec2edf780268
vipulsha <sharma.vipul.999@gmail.com>
2018-01-18 15:00:50 +0530

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -7,23 +7,6 @@ import java.util.Properties;
 
 public class Test {
 	
-	static int a=10;
-	int b;
-	
-	public Test() {
-		a = 20;
-	}
-	
-	@org.testng.annotations.Test
-	public void test() {
-		System.out.println();
-	}
-	
-	public static void test2() {
-	}
-	
-	
-	
 	public static void main(String[] args) {
 	    System.out.println("Shree Ganeshay Namah");
 	}
